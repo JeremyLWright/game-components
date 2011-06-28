b8b61d28e275586f237983fce3a2909a9e0c2063
Jeremy Wright <jeremy@quiescent.us>
2011-06-27 23:17:01 -0700

    started a basic test.

--- models/tests/BasicTest.cpp
+++ models/tests/BasicTest.cpp
@@ -1,7 +1,21 @@
 #include <gtest/gtest.h>
 #include <limits.h>
+#include "Graph.h"
 
-TEST(BasicTest, One)
+using Practicum::Models::Graph;
+
+TEST(Models, One)
 {
-    EXPECT_EQ(1,1);
+    Graph::Ptr g = Graph::construct();
+
+    g->addNode("Frodo");
+    g->addNode("Gandalf");
+    g->addNode("Pip");
+    g->addNode("Strider");
+    
+    g->addDirectedEdge("Frodo", "Gandalf", 1);
+    g->addDirectedEdge("Frodo", "Pip", 1);
+    g->addDirectedEdge("Frodo", "Strider", 1);
+    g->addDirectedEdge("Gandalf", "Pip", 1);
+    g->addDirectedEdge("Pip", "Strider", 1);
 }
