a2705daebc28f8fc24fa086200a48cb4812feff5
Jeremy Wright <jeremy@quiescent.us>
2011-07-02 16:14:08 -0700

    A super basic ConsoleView implementation.

--- main.cpp
+++ main.cpp
@@ -6,8 +6,25 @@
  * Creative Commons Attribution-ShareAlike 3.0 Unported License.
  */
 
+#include "views/ConsoleView.h"
+#include <vector>
+#include <string>
+using std::string;
+using std::vector;
+using namespace Practicum;
+
 int main(int argc, const char *argv[])
 {
-    
+    View::ConsoleView::Ptr userConsole = View::ConsoleView::construct();
+    userConsole->SendMessage("Welcome to the C++ Choose Your Own Adventure.");
+    vector<string> menu;
+    menu.push_back("Load Game.");
+    menu.push_back("Quit");
+    do {
+        if(userConsole->GetUserOption(menu) == 1)
+        return 0;
+    } while(1);
+
+
     return 0;
 }
