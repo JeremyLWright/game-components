8e8733880ed3363fa4c703e8480f189f72772af0
Jeremy Wright <jeremy@quiescent.us>
2011-07-10 13:47:20 -0700

    Moved the agent coupling to the CommandFactory, instead of with the Parser. It is a little better.

--- prototype/Parser.py
+++ prototype/Parser.py
@@ -5,23 +5,7 @@ class Parser():
         self.verbs = ("walk","take","lick","touch","look",)
         self.directions = ("north", "south", "east", "west",)
 
-    def GetLine(self, agent):
+    def GetLine(self):
         line = raw_input("?> ")
         items = line.split()
-        verb = items[0]
-        if(verb == "exit"):
-            return ICommand.ExitCommand()
-        noun = items[1]
-        if(verb == "walk"):
-            if(noun == "north"):
-                return ICommand.GoNorthCommand(agent)
-            elif(noun == "south"):
-                return ICommand.GoSouthCommand(agent)
-            elif(noun == "east"):
-                return ICommand.GoEastCommand(agent)
-            elif(noun == "west"):
-                return ICommand.GoWestCommand(agent)
-            else:
-                print "Invalid Direction"
-
-
+        return items
