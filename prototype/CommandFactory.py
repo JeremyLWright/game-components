fa96057bf44ec83429c457cb6020d7a1596f0e8b
Jeremy Wright <jeremy@quiescent.us>
2011-07-10 14:25:40 -0700

    Added a json backend.

---
+++ prototype/CommandFactory.py
@@ -0,0 +1,27 @@
+import ICommand
+
+class CommandFactory():
+    def __init__(self, agent):
+        self.agent = agent
+
+    def GetCommand(self, parser_tokens):
+        verb = parser_tokens[0]
+        if(verb == "exit"):
+            return ICommand.ExitCommand()
+        elif(verb ==  "look"):
+            return ICommand.AgentLookCommand(self.agent)
+        noun = parser_tokens[1]
+        if(verb == "walk"):
+            if(noun == "north"):
+                return ICommand.GoNorthCommand(self.agent)
+            elif(noun == "south"):
+                return ICommand.GoSouthCommand(self.agent)
+            elif(noun == "east"):
+                return ICommand.GoEastCommand(self.agent)
+            elif(noun == "west"):
+                return ICommand.GoWestCommand(self.agent)
+            else:
+                print "Invalid Direction"
+        
+
+
