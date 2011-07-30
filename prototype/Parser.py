1471eed37a84bfda9c3841d659eb4eb9c6922431
Jeremy Wright <jeremy@quiescent.us>
2011-07-30 14:10:45 -0700

    Added the parser to the Agent Class.

--- prototype/Parser.py
+++ prototype/Parser.py
@@ -3,8 +3,10 @@ import ICommand
 class Parser():
     def __init__(self):
         pass
+    def __init__(self, owner):
+        self.owner = owner
 
     def GetLine(self):
-        line = raw_input("?> ")
+        line = raw_input("(%s) ?> "%(self.owner.name))
         items = line.split()
         return items
