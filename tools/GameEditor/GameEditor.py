1172f9fe3b35129391798d000d499181cffdebfb
Jeremy Wright <jeremy@codestrokes.com>
2011-08-19 21:38:20 -0700

    UI loads locations from the json database.

--- tools/GameEditor/GameEditor.py
+++ tools/GameEditor/GameEditor.py
@@ -2,6 +2,7 @@ import sys
 from PyQt4 import QtGui,QtCore
 from UI import *
 import NewCharacter
+import json
 
 class GameEditor(QtGui.QMainWindow):
     def __init__(self, parent=None):
@@ -9,12 +10,43 @@ class GameEditor(QtGui.QMainWindow):
         QtGui.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        QtCore.QObject.connect(self.ui.pushButton_NewCharacter, QtCore.SIGNAL('clicked()'), self.NewCharacter)
-        QtCore.QObject.connect(self.ui.pushButton_CommitDialogText, QtCore.SIGNAL('clicked()'), self.CommitDialogText)
-        QtCore.QObject.connect(self.ui.pushButton_NewTrigger, QtCore.SIGNAL('clicked()'), self.NewTrigger)
+        data = open("../../prototype/map.json", "rw")
+        self.db = json.load(data)
+
+        QtCore.QObject.connect(self.ui.buttonSaveDescription, QtCore.SIGNAL('clicked()'), self.CommitDialogText)
+        QtCore.QObject.connect(self.ui.buttonNewLocation, QtCore.SIGNAL('clicked()'), self.NewLocation)
+        QtCore.QObject.connect(self.ui.comboBoxNorthExit, QtCore.SIGNAL('currentIndexChanged(QString)'), self.NorthExitChanged)
+        QtCore.QObject.connect(self.ui.comboBoxSouthExit, QtCore.SIGNAL('currentIndexChanged(QString)'), self.SouthExitChanged)
+        QtCore.QObject.connect(self.ui.comboBoxEastExit, QtCore.SIGNAL('currentIndexChanged(QString)'), self.EastExitChanged)
+        QtCore.QObject.connect(self.ui.comboBoxWestExit, QtCore.SIGNAL('currentIndexChanged(QString)'), self.WestExitChanged)
+
+        for room in self.db.keys():
+            self.ui.comboBoxNorthExit.addItem(room)
+            self.ui.comboBoxSouthExit.addItem(room)
+            self.ui.comboBoxEastExit.addItem(room)
+            self.ui.comboBoxWestExit.addItem(room)
+            self.ui.listWidgetLocations.addItem(room)
 
     def CommitDialogText(self):
         print "Saving Dialog to Database."
+
+    def SouthExitChanged(self, string):
+        print "South Changed"
+
+    def EastExitChanged(self, string):
+        print "East Changed"
+
+    def WestExitChanged(self, string):
+        print "West Changed"
+
+    def NorthExitChanged(self, string):
+        print "North Changed"
+
+    def NewLocation(self):
+        print "New Trigger Added."
+
+    def NewLocationExit(self):
+        print "New Trigger Added."
 
     def NewTrigger(self):
         print "New Trigger Added."
