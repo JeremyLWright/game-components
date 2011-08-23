567bcd83780159e1e56fe1541e540ab7f6027ea6
Jeremy Wright <jeremy@codestrokes.com>
2011-08-22 20:16:54 -0700

    Simplified the exit drawing.

--- tools/draw_map.py
+++ tools/draw_map.py
@@ -12,18 +12,10 @@ data.close()
 graph = pydot.Dot('graphname', graph_type='digraph')
 
 for room in db.keys():
-    if(db[room]["Exits"]["East"] != ""):
-        e = pydot.Edge(room, db[room]["Exits"]["East"],label=str("East"))
-        graph.add_edge(e)
-    if(db[room]["Exits"]["West"] != ""):
-        e = pydot.Edge(room, db[room]["Exits"]["West"],label=str("West"))
-        graph.add_edge(e)
-    if(db[room]["Exits"]["North"] != ""):
-        e = pydot.Edge(room, db[room]["Exits"]["North"],label=str("North"))
-        graph.add_edge(e)
-    if(db[room]["Exits"]["South"] != ""):
-        e = pydot.Edge(room, db[room]["Exits"]["South"],label=str("South"))
-        graph.add_edge(e)
+    for exit in db[room]["Exits"].keys():
+        if(db[room]["Exits"][exit] != ""):
+            e = pydot.Edge(room, db[room]["Exits"][exit],label=str(exit))
+            graph.add_edge(e)
 
 graph.write_svg("mygraph.svg")
 
